b0bfde68fe2c361e0916940914168470b6174394
buzunser <79738404+buzunser@users.noreply.github.com>
2022-07-17 21:07:04 +0300

    sm6150-common: Move to common fingerprint HIDL

--- sm6150.mk
+++ sm6150.mk
@@ -140,6 +140,9 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor
 
 # Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
