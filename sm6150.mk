f03f95408e97c84a92367e4f40ccc15e079edfd5
buzunser <79738404+buzunser@users.noreply.github.com>
2022-07-24 19:41:53 +0300

    Update sm6150.mk

--- sm6150.mk
+++ sm6150.mk
@@ -23,6 +23,9 @@ $(call inherit-product, vendor/xiaomi/sm6150-common/sm6150-common-vendor.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# ThemeIcons
+$(call inherit-product, packages/overlays/ThemeIcons/config.mk)
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
